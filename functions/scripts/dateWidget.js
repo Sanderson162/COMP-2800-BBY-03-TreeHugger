37f087881f24755e26a276c5ec97da71d13f9c96
Steven Reid <sreid97@my.bcit.ca>
2021-05-07 15:04:24 -0700

    Co-authored-by: AidanMcReynolds <AidanMcReynolds@users.noreply.github.com> Co-authored-by: Sanderson162 <Sanderson162@users.noreply.github.com> Co-authored-by: Amrit Manhas <amritmanhas11@gmail.com>

--- functions/scripts/dateWidget.js
+++ functions/scripts/dateWidget.js
@@ -13,10 +13,6 @@ function updateDateBlock(date) {
     }));
 }
 
-$(document).ready(function () {
-    updateDateBlock(new Date());
-});
-
 function searchResults(entry) {
     let newEntry = "<div class='card'>";
     newEntry += "<p>" + entry.fields.genus_name + " " + entry.fields.species_name + "</p>";
@@ -28,7 +24,6 @@ function searchResults(entry) {
 
 function changeDate(date) {
     $.getJSON('https://opendata.vancouver.ca/api/records/1.0/search/?dataset=street-trees&q=date_planted+%3D+' + $.datepicker.formatDate("yy-mm-dd", date).toString() + '&lang=en&start=1', function (data) {
-        console.log(data.records.length);
         if (data.records.length == 0) {
             $("#main").html("<i>No results found...</i>");
         } else {
@@ -41,14 +36,37 @@ function changeDate(date) {
     updateDateBlock(date);
 }
 
+//https://stackoverflow.com/questions/2385332/highlight-dates-in-specific-range-with-jquerys-datepicker
+
 $(function () {
+    updateDateBlock(new Date());
+
+    let dates = [];
+    let query = "https://opendata.vancouver.ca/api/v2/catalog/datasets/street-trees/facets?facet=date_planted&refine=date_planted%3A2019%2F03&timezone=UTC";
+    $.getJSON(query, (data) => {
+        dates = data.facets[0].facets[0].facets[0].facets;
+    });
+
+    dates = ["2021/05/20", "2021/05/21"];
+
     $('#datepicker').datepicker({
         inline: true,
         showOtherMonths: true,
         dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
         altField: "#date",
+        beforeShowDay: highlightDays,
         onSelect: function (dateText, inst) {
             changeDate($(this).datepicker("getDate"));
+            $( "#datepicker" ).datepicker("refresh");
         }
     });
+
+    function highlightDays(date) {
+        for (var i = 0; i < dates.length; i++) {
+            if (new Date(dates[i]).toString() == date.toString()) {
+                return [true, 'event', "Trees planted on this date!"];
+            }
+        }
+        return [true, ''];
+     }
 });
