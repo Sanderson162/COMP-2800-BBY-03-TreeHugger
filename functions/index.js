036d0b6d14ffdc52754768eb78abd98d88331447
apsm100 <amritmanhas11@gmail.com>
2021-05-14 14:18:42 -0700

    Navbar update

--- functions/index.js
+++ functions/index.js
@@ -478,9 +478,9 @@ app.get('/timestamp', function (req, res) {
 });
 
 
-// app.listen(PORT, () => {
-//      console.log(`Listening on http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+     console.log(`Listening on http://localhost:${PORT}`);
+});
 
 
 function msg404(res) {
