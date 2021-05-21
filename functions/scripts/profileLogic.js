c09a1fc6d96c0864605a54a5a7e652cdd10a6ae6
Sanderson162 <sanderson162@my.bcit.ca>
2021-05-21 11:53:55 -0700

    profile now shows user info

--- functions/scripts/profileLogic.js
+++ functions/scripts/profileLogic.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
   $("#userNameArea").on('click', 'span', function () {
+    console.log("clicked on username");
     let td = $(this).parent();
     let spanText = td.children().text();
     let input = $("<input type='text' value='" + spanText + "'>");
@@ -21,6 +22,50 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      getUserInfo();
+    } else {
+      $("#userNameArea").children().text("");
+      $("#emailArea").text("");
+    }
+  });
+
+  function getUserInfo() {
+    var user = firebase.auth().currentUser;
+    if (user) {
+      firebase.auth().currentUser.getIdToken(true).then(function (idToken) {
+        $.ajax({
+          url: "/profile",
+          dataType: "json",
+          type: "POST",
+          data: {
+            idToken: idToken
+          },
+          success: function (result, status, xhr) {
+            if (status == 'success') {
+              console.log("Profile retrieved");
+              if(!result.name) {
+                $("#userNameArea").children().text("Set username here");
+              } else {
+                $("#userNameArea").children().text(result.name);
+              }
+              
+              $("#emailArea").text(result.email);
+            } else {
+              console.log("unable to retrieve");
+            }
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log("ERROR:", jqXHR, textStatus, errorThrown);
+          }
+
+        });
+      });
+    } else {
+      console.log("Not signed in");
+    }
+  }
 
 
   function updateUsername(newUsername) {
