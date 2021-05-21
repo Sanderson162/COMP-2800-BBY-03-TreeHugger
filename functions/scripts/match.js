c02ddbe598b2da5076275ecd7d69592b543caf03
Aidan McReynolds <akmcr@yahoo.ca>
2021-05-21 14:15:31 -0700

    added documentation to match.js

--- functions/scripts/match.js
+++ functions/scripts/match.js
@@ -1,3 +1,4 @@
+//Loads the event listeners for the menu buttons. 
 $(document).ready(() => {
     $("#form").hide();
     $("#birth").click(() => {
@@ -16,10 +17,14 @@ $(document).ready(() => {
         loadEventForm()
     });
 });
+
+//highlights a selected card, unhighlights the others
 function selectCard(target) {
     $(".selected").removeClass("selected")
     $(target).addClass("selected")
 }
+
+//loads the form for birth tree
 function loadBirthForm() {
     $("#result").html("");
     let f = $("#form").html("");
@@ -38,6 +43,8 @@ function loadBirthForm() {
     }));
     f.append($("<span id='formmsg'>"));
 }
+
+//loads the form for the anniversary tree
 function loadAnniversaryForm() {
     $("#result").html("");
     let f = $("#form").html("");
@@ -58,6 +65,8 @@ function loadAnniversaryForm() {
     }));
     f.append($("<span id='formmsg'>"));
 }
+
+//loads the form for event tree
 function loadEventForm() {
     $("#result").html("");
     let f = $("#form").html("");
@@ -76,12 +85,17 @@ function loadEventForm() {
     }));
     f.append($("<span id='formmsg'>"));
 }
+
+//click event for form submission
 function submit(type) {
+    //form validation
     if (!validForm()){
         $("#formmsg").html("Invalid input, try again");
         return;
     }
     $("#formmsg").html("")
+
+    //query by date (find exact match)
     let q = $("#date").val();
     console.log(q.toString());
     let query = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=street-trees&q=&facet=genus_name&facet=species_name&facet=common_name&facet=assigned&facet=root_barrier&facet=plant_area&facet=on_street&facet=neighbourhood_name&facet=street_side_name&facet=height_range_id&facet=curb&facet=date_planted&refine.date_planted=" + q.toString();
@@ -92,12 +106,14 @@ function submit(type) {
             $("#result").html("")
             $("#result").append(displayTree(x,type.match,saveButton(x, type.message, type.emoji)));
         } else {
+            // no exact match
             alternateTree(q, type);
 
         }
     });
 }
 
+//performs form validation
 function validForm(){
     if ($("#name").length && !($("#name").val())){
         console.log("invalid name")
@@ -111,6 +127,7 @@ function validForm(){
         console.log("invalid name2")
         return false;
     }
+    //checks for ####-##-##
     const regex = /^\d{4}-\d{2}-\d{2}$/;
     if (!($("#date").val().match(regex))){
         console.log($("#date").val())
@@ -120,21 +137,26 @@ function validForm(){
     return true;
 }
 
+//looks for an alternate tree if there is no exact match
 async function alternateTree(q, type){
+    //find one in the same month
     let x = await findClosest(q, type);
     console.log(x);
     if (x) {
         return;
     }
+    //find with a different year but the same day and month
     x = await differentYear(q, type)
     if (x){
         return;
     }
+    //no result found
     $("#result").html("");
     $("#result").append($("<div class='card'>)").append(type.fail));
 }
 
-
+//finds a tree between 1989 and 2022 with the same day and month
+//returns the first tree it finds. 
 async function differentYear(date, type){
     let monthDay = date.toString().substring(4);
     let x = [];
@@ -155,9 +177,9 @@ async function differentYear(date, type){
         } 
     }
     return false;
-    
-
 }
+
+//finds the trees in the same month
 async function findClosest(date, type) {
     let month = date.toString().substring(0, 7);
     let day = date.toString().substring(8);
@@ -178,6 +200,9 @@ async function findClosest(date, type) {
     return x;
 }
 
+//given trees in the same month, finds the closest. 
+//if there are multiple closest trees, 
+//returns a random tree among the closest
 function closestTree(data, month, day) {
     let d = parseInt(day);
     let x = [];
@@ -199,6 +224,7 @@ function closestTree(data, month, day) {
     console.log("error");
 }
 
+//displays the tree
 function displayTree(entry,message,saveBtn) {
     let elem = $("<div></div>").addClass("card");
     elem.append(message);
@@ -208,11 +234,11 @@ function displayTree(entry,message,saveBtn) {
     elem.append($("<p></p>").html("Planted on: " + entry.fields.date_planted));
     elem.append(viewButton(entry.recordid));
     elem.append(saveBtn);
-    
 
     return elem;
 }
 
+//returns a button with a link to the tree
 function viewButton(tree){
     let btn = $("<button id='save'>View</button>");
     btn.click(() =>{
@@ -221,6 +247,8 @@ function viewButton(tree){
     return btn
 }
 
+//returns a save button
+//if the user is not logged in it displays a warning
 function saveButton(tree, message, emoji) {
     let btn = $("<button id='save'>Save</button>");
     btn.on("click", () => {
